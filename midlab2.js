b58911825f88e594561562b1151734b31161a8c7
MD.Sadman Sanim <sadmansanim787@gmail.com>
2025-08-06 12:22:24 +0600

    partial

--- midlab2.js
+++ midlab2.js
@@ -1,3 +1,4 @@
-<script>
-document.getElementById("demo").innerHTML = "Java";
-</script>
+// Generate a random number between 1 and 100
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+console.log("Your random number is: " + randomNumber);
