65f0c5261a9cfda3a8ccde100fec00e872e98ff7
MD.Sadman Sanim <sadmansanim787@gmail.com>
2025-08-06 12:35:10 +0600

    second

--- midlab2.js
+++ midlab2.js
@@ -1,4 +1,3 @@
-// Generate a random number between 1 and 100
-let randomNumber = Math.floor(Math.random() * 100) + 1;
-
-console.log("Your random number is: " + randomNumber);
+<script>
+document.getElementById("demo").innerHTML = "My First JavaScript";
+</script>
